bf90f978d3fea80495d3bcbc8c258026d611c70b
shaikh-zaiid <zaidshaikh2766@gmail.com>
2024-09-23 18:38:22 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./assets/css/style.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import Products from "./components/Products";
+import Review from "./components/Review";
+import Contact from "./components/Contact";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Home />
+      <About />
+      <Menu />
+      <Products />
+      <Review />
+      <Contact />
+      <Blog />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <>
+      <section class="contact" id="contact">
+        <h1 class="heading">
+          <span>contact</span> us
+        </h1>
+        <div class="row">
+          <iframe
+            class="map"
+            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Ahmedabad+(Restaurant)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            allowfullscreen=""
+            loading="lazy"
+          ></iframe>
+
+          <form action="">
+            <h3>get in touch</h3>
+            <div class="inputBox">
+              <span class="fas fa-user"></span>
+              <input type="text" placeholder="name" />
+            </div>
+            <div class="inputBox">
+              <span class="fas fa-envelope"></span>
+              <input type="email" placeholder="email" />
+            </div>
+            <div class="inputBox">
+              <span class="fas fa-phone"></span>
+              <input type="number" placeholder="number" />
+            </div>
+            <input type="submit" value="contact now" class="btn" />
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Contact;
